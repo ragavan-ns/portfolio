5863286fded984d674e33121d0d3d0ee12460963
ragavan-ns <132727152+ragavan-ns@users.noreply.github.com>
2023-09-03 07:23:54 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/projects/phonePe.png'
+import RealEst from '../assets/projects/dice.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Virtual Dice Roller Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"PhonePe Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    // {
+    //     id:3,
+    //     name:"React JS Application",
+    //     image:WorkIm,
+    //     github:"",
+    //     live:"",
+    // },
+    // {
+    //     id:4,
+    //     name:"React JS Application",
+    //     image:RealEst,
+    //     github:"",
+    //     live:"",
+    // },
+
+
+]
