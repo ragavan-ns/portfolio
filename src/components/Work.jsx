fe7d9973cc6ae8a977a06d3c1a511d95c386e2d7
ragavan-ns <132727152+ragavan-ns@users.noreply.github.com>
2023-09-04 19:06:56 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -44,7 +44,7 @@ const Work = () => {
              Code
           </button>
         </a>
-        {/* eslint-disable-next-line {item.live} */}
+        {/* eslint-disable-next-line */}
            <a href={item.live} target="_blank">          
                <button className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
