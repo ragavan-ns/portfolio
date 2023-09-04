8b9d7beed2953c1d4dadc0ce40dab96a22ad498e
ragavan-ns <132727152+ragavan-ns@users.noreply.github.com>
2023-09-04 10:23:22 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -7,15 +7,15 @@ export const data=[
         name:"Virtual Dice Roller Application",
         image:RealEst,
         github:https://www.github.com/ragavan-ns/Virtual-Dice-Roller-Application,
-        live:https://ragavan-ns.github.io/Virtual-Dice-Roller-Application/,
+        live:https://ragavan-ns.github.io/Virtual-Dice-Roller-Application/
     },
     {
         id:2,
         name:"PhonePe Application",
         image:WorkIm,
         github:https://www.github.com/ragavan-ns/Virtual-Dice-Roller-Application,
-        live:https://ragavan-ns.github.io/Virtual-Dice-Roller-Application/,
-    },
+        live:https://ragavan-ns.github.io/Virtual-Dice-Roller-Application/
+    }
     // {
     //     id:3,
     //     name:"React JS Application",
