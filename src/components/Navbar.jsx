96ff0061703142825c0fa0aa673521fb8d004c31
ragavan-ns <132727152+ragavan-ns@users.noreply.github.com>
2023-09-06 13:37:56 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -126,7 +126,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
           <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='mailto:ragavanmaathavan@gmail.com?'
+              href='https://ragavan-ns.github.io/portfolio/'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a> 
